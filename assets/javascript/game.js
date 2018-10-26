b8e42496630c05609cd15b0e3301cffeb5738b17
GitWilder <43479032+Wilder101@users.noreply.github.com>
2018-10-26 13:37:42 -0700

    Backup before switch to CrystalsCollector Game

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,55 +1,92 @@
 // unit-4-game -- Star Wars Game by WBM, 10/23/18, javascript file 
 
-// Variables
+$(document).ready(function() {
+
+    // Variables
+    let gameStateRoleSelection     = true;
+    let gameStateOpponentSelection = false;
+
+    // Array of (literal) roles that can be played -- see what I did there?
+    var characters = [
+        {name: "Luke Skywalker", hp: 10, player: true , imgURL: "assets/images/luke-skywalker.jpg"},
+        {name: "Yoda"          , hp: 11, player: false, imgURL: "assets/images/yoda.jpg"},
+        {name: "Boba Fett"     , hp: 10, player: false, imgURL: "assets/images/boba-fett.jpg"},
+        {name: "Darth Vader"   , hp: 10, player: false, imgURL: "assets/images/darth-vader.jpg"}
+        
+    ];
 
 
 
-// // Struct-like creater function
-// function Role(name, power, player) {
-//     this.name = name;
-//     this.power = power;
-//     this.player = player;
-// };
+    if (gameStateRoleSelection) {
 
-// // Array of roles that can be played 
-// var characters = [
-//     new Role("Luke Skywalker", 10, true),
-//     new Role("Yoda"          , 11, false),
-//     new Role("Boba Fett"     , 12, false),
-//     new Role("Darth Vader"   , 13, false)
-// ];
+        // On page load, show the roles available
+        for (let i = 0; i < characters.length; i++) {
+            // Testing
+            console.log(characters[i].name + " " + characters[i].hp + " " + characters[i].player);
 
-var characters = [
-    {name: "Luke Skywalker", power: 10, player: true},
-    {name: "Yoda"          , power: 11, player: false},
-    {name: "Boba Fett"     , power: 10, player: false},
-    {name: "Darth Vader"   , power: 10, player: false}
+            // // Create a variable named "imgShow" equal to $("<div>");
+            // var imgShow = $("<div>");
+
+            // // Give each "imgShow" these classes
+            // imgShow.addClass("d-inline img-container");
+
+            // // Give each "imgShow" a data-attribute called "data-character".
+            // imgShow.attr("data-character", i);
+
+            // // 5. Then give each "letterBtns" a text equal to "letters[i]".
+            // imgShow.text(characters[i].name);
+
+            // //<div id="your-character"></div>
+            // // 6. Finally, append each "letterBtn" to the "#buttons" div (provided).
+            // $("#your-character").append(imgShow);
+
+            // $("div.img-container").html("<img src=\"assets/images/luke-skywalker.jpg\" alt=\"Luke Skywalker\"><div class=\"char-name\">Luke Skywalker</div><div class=\"health-points\">Health Points: XXX</div>");
+
+            // WORKING HERE... PUT IN THE IMAGE LOCATION URL VARIABLE! ----- use the ' instead of the escape\"", man!!!
+            // $(".img-container").html( 
+            //     '<img src=' + '"'  + characters[i].imgURL + '"' + '  alt=' + '"' + characters[i].name + '>' + 
+            //     '<div class=' + '"char-name">' + characters[i].name + '</div>' + 
+            //     '<div class=' + '"health-points">' + 'Health Points: ' + characters[i].hp + '</div>');
+
+                // $("#laps").append(stopwatch.time + "<br>");
+
+            // // Build a character container div for grouped role data display tile
+            // var characterContainer = $("<div>");
+            // characterContainer.addClass("d-inline img-container");
+
+
+
+
+
+            //     $("#your-character").append('<img src="assets/images/luke-skywalker.jpg" alt="Luke Skywalker">');
+            //     $("#your-character").append('<div class="char-name">Luke Skywalker</div>');
+            //     $("#your-character").append('<div class="health-points">Health Points: XXX</div>');
+
+                // REFERENCE
+                // var audioElement = document.createElement("audio");
+                // audioElement.setAttribute("src", "assets/captainplanet24.mp3");
+
+            // <div class="d-inline img-container">
+            //     <img src="assets/images/luke-skywalker.jpg" alt="Luke Skywalker">
+            //     <div class="char-name">Luke Skywalker</div>
+            //     <div class="health-points">Health Points: XXX</div>
+            // </div>
+
+            // The content of <div class="demo-container"> can be set like this:
+            // $( "div.demo-container" )
+            // .html( "<p>All new content. <em>You bet!</em></p>" );
+
+
+            }
+
+    }
+
+
     
-];
-
-// var testArray = [
-//     {name: "Dave", power: 20, player: true},
-//     {name: "Bill", power: 21, player: false},
-//     {name: "Hash", power: 22, player: false},
-//     {name: "Bobb", power: 23, player: false}
-// ];
-
-// On page load, show the roles available
-for (let i = 0; i < characters.length; i++) {
-    // Testing
-    console.log(characters[i].name + " " + characters[i].power + " " + characters[i].player);
-
-}
-
-// Testing literal version of array
-// for (let i = 0; i < testArray.length; i++) {
-//     // Testing
-//     console.log(testArray[i].name + " " + testArray[i].power + " " + testArray[i].player);
-
-// }
 
 
 
 
 
 
+}); // End document.ready
