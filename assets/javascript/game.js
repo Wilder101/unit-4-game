352bfe73f4e2e2be70e44bef5fe75a0b9558faf5
GitWilder <43479032+Wilder101@users.noreply.github.com>
2018-10-27 20:03:05 -0700

    UI drafted well

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,12 +3,52 @@
 $(document).ready(function() {
 
     // Variables
-    
+    let randomNumToGuess = 0;
+    let wins = 0;
+    let losses = 0;
+    let totalScore = 0;
+
+    // Array of (literal) gems that can be played -- see what I did there?
+    var gems = [
+        {name: "orange",  randomHiddenValue: 0},    // BETWEEN 1 - 12
+        {name: "pink",    randomHiddenValue: 0},
+        {name: "purple",  randomHiddenValue: 0},
+        {name: "yellow" , randomHiddenValue: 0}
+    ];
+
+    // Set up a fresh game to play
+    function initializeGame () {
+        // Set random hidden values of all gems
+        for (let i = 0; i < gems.length; i++) {
+            gems[i].randomHiddenValue = setRandomHiddenValue();
+        }
+
+        // Set up the random number to be guessed
+        randomNumToGuess = setRandomNumToGuess();
+        
+        // Update variable
+        totalScore = 0;
 
 
+        // Update UI...
+    };
+
+    // Returns a random number between 1 - 12
+    function setRandomHiddenValue() {
+        let random = 5;     // CHANGE TO MATH.FLOOR/MATH.RANDOM GENERATOR FOR 1 - 12 RESULT
+        return random;
+    };
+
+    // Returns a random number between 19 - 120
+    function setRandomNumToGuess() {
+        let random = 40;    // CHANGE TO MATH.FLOOR/MATH.RANDOM GENERATOR FOR 19 - 120 RESULT
+        return random;
+    };
 
 
-
+    // On page load
+    randomNumToGuess = setRandomNumToGuess();
+    initializeGame();
 
     // Events
 
